636e035c77418318690cccdf14c4516053fdc1cf
HenrikStade <henrik.std@gmail.com>
2022-06-25 07:27:26 +0200

    pong start

---
+++ pong/main.py
@@ -0,0 +1,23 @@
+from turtle import Turtle, Screen
+
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 600
+
+screen = Screen()
+screen.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
+screen.bgcolor("black")
+
+timmy = Turtle()
+
+
+
+screen.exitonclick()
+
+# TODO: make a paddle class
+# TODO: make a ball class
+# TODO: make a scoreboard
+# TODO: setup the screen
+# TODO: collision with paddles
+# TODO: collision with upper / lower wall
+# TODO: increase score if past opponents paddle
+5
