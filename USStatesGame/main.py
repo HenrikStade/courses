e46b72ef6ec2db75fb27699881c5353cba3ad5ea
HenrikStade <henrik.std@gmail.com>
2022-06-28 16:51:15 +0200

    Day 25 complete

--- USStatesGame/main.py
+++ USStatesGame/main.py
@@ -1,24 +1,37 @@
+import turtle
+
 import pandas as pd
 from turtle import Turtle, Screen
 
 screen = Screen()
-screen.bgpic("blank_states_img.gif")
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
 
 df = pd.read_csv("50_states.csv")
 
-user_guess = screen.textinput(title="", prompt="Name an American state:").lower()
-print(user_guess)
+all_states = df["state"].tolist()
 
-state = df[df["state"]. == "alabama"]
-print(state)
-state_pos = (int(state.x), int(state.y))
+while len(all_states) > 0:
+    user_guess = screen.textinput(title="", prompt="Name an American state:").title()
+    if user_guess == "Exit":
+        df = pd.DataFrame(all_states)
+        df.to_csv("missing_states.csv")
+        exit()
+    if user_guess in all_states:
+        state = df[df.state == user_guess]
 
-timmy = Turtle()
-timmy.penup()
-timmy.write(str(state.iloc[0]["state"]))
-timmy.hideturtle()
-timmy.goto(state_pos)
+        state_pos = (int(state.x), int(state.y))
 
+        t = Turtle()
+        t.penup()
+        t.hideturtle()
+        t.goto(state_pos)
+        t.write(state.state.item())  # user_guess)#str(state.iloc[0]["state"]))
 
+        all_states.remove(user_guess)
+    # else:
+    #     user_guess = screen.textinput(title="", prompt="Name an American state:").capitalize()
 
 screen.exitonclick()
