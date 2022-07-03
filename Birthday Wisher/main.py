b2103a0b3fc8645a168444aae40dc5f99386b5ed
HenrikStade <henrik.std@gmail.com>
2022-07-03 10:10:27 +0200

    Day 32 complete

---
+++ Birthday Wisher/main.py
@@ -0,0 +1,31 @@
+
+import datetime as dt
+import random
+import smtplib
+
+my_email = "springtiden@gmail.com"
+my_password = "ueLMR98j7939"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=my_password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="nipptiden@gmail.com",
+        msg="Subject:Hello\n\nThis is the body of my email.")
+
+now = dt.datetime.now()
+year = now.year
+day = now.day
+day_of_week = now.weekday()
+
+date_of_birth = dt.datetime(year=1987, month=9, day=13)
+
+
+with open("quotes.txt", "r") as f:
+    quotes_list = f.readlines()
+
+if day_of_week == 1:
+    quote = random.choice(quotes_list)
+
+
